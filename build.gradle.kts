feff56f36ff4dda9bd208b58c9ec20c62bdb7373
uiytt <uiytt@protonmail.com>
2022-04-26 12:02:59 +0200

    Automatic ressource processing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.6.20"
+    kotlin("jvm") version "1.6.21"
 }
 
 group = "fr.uiytt"
@@ -16,7 +16,15 @@ repositories {
 
 dependencies {
     compileOnly("org.spigotmc:spigot-api:1.18.2-R0.1-SNAPSHOT")
-    implementation(kotlin("stdlib-jdk17"))
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.6.21")
     implementation("com.github.simplix-softworks:simplixstorage:3.2.4")
     implementation("fr.minuskube.inv:smart-invs:1.2.7")
+}
+
+tasks.processResources {
+    //inputs.property "version", project.version
+
+    filesMatching("plugin.yml") {
+        expand(mapOf("version" to project.version) )
+    }
 }
