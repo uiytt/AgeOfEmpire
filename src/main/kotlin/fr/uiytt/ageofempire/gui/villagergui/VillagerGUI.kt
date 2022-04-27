ee87a41c465f550a56dc6db9d1cb4bd3182b037e
uiytt <uiytt@protonmail.com>
2022-04-27 18:12:19 +0200

    Minor fix

--- src/main/kotlin/fr/uiytt/ageofempire/gui/villagergui/VillagerGUI.kt
+++ src/main/kotlin/fr/uiytt/ageofempire/gui/villagergui/VillagerGUI.kt
@@ -5,6 +5,7 @@ import fr.minuskube.inv.content.InventoryContents
 import fr.minuskube.inv.content.InventoryProvider
 import fr.uiytt.ageofempire.game.GameData
 import fr.uiytt.ageofempire.game.getGameManager
+import fr.uiytt.ageofempire.game.getPlayerTeam
 import fr.uiytt.ageofempire.gui.GUIUtils.initGui
 import org.bukkit.entity.Player
 
